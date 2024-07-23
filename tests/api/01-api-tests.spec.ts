32d0b4fd335e8c36e6434143f3b2b89794c28465
josh <66197125+justjoshn@users.noreply.github.com>
2024-07-23 12:00:08 -0700

    Get All Brands List + POST To Search Product api tests

--- tests/api/01-api-tests.spec.ts
+++ tests/api/01-api-tests.spec.ts
@@ -1,7 +1,12 @@
-import { test, expect, request } from '@playwright/test'
+import { test, expect } from '@playwright/test'
+import { faker } from '@faker-js/faker'
+
+const productsList = '/api/productsList'
+const brandsList = '/api/brandsList'
+const searchProduct = '/api/searchProduct'
 
 test('Get All Products List', async ({ request }) => {
-  const response = await request.get('/api/productsList')
+  const response = await request.get(productsList)
   const jsonResponse = await response.json()
 
   expect(response.status()).toBe(200)
@@ -27,5 +32,58 @@ test('Get All Products List', async ({ request }) => {
     expect(['Tops', 'Tshirts', 'Dress', 'Tops & Shirts', 'Jeans', 'Saree']).toContain(
       product.category.category
     )
+  })
+})
+
+test('Get All Brands List', async ({ request }) => {
+  const response = await request.get(brandsList)
+  const jsonResponse = await response.json()
+
+  expect(response.status()).toBe(200)
+  expect(jsonResponse).toHaveProperty('brands')
+  expect(Array.isArray(jsonResponse.brands)).toBe(true)
+  expect(jsonResponse.brands.length).toBeGreaterThan(0)
+
+  jsonResponse.brands.forEach(brand => {
+    expect(brand).toHaveProperty('id')
+    expect(typeof brand.id).toBe('number')
+    expect(brand).toHaveProperty('brand')
+    expect(typeof brand.brand).toBe('string')
+
+    expect([
+      'Polo',
+      'H&M',
+      'Madame',
+      'Mast & Harbour',
+      'Babyhug',
+      'Allen Solly Junior',
+      'Kookie Kids',
+      'Biba',
+    ]).toContain(brand.brand)
+  })
+})
+
+test('POST To Search Product', async ({ request }) => {
+  const getAllProductsResponse = await request.get(productsList)
+  const products = await getAllProductsResponse.json()
+
+  expect(getAllProductsResponse.ok()).toBeTruthy()
+
+  const randomIndex: number = faker.number.int({ min: 0, max: products.products.length - 1 })
+  const randomProduct = products.products[randomIndex].name
+
+  const searchResponse = await request.post(searchProduct, {
+    form: {
+      search_product: randomProduct,
+    },
+  })
+
+  expect(searchResponse.status()).toBe(200)
+
+  const searchResults = await searchResponse.json()
+
+  searchResults.products.forEach(product => {
+    expect(product).toHaveProperty('name')
+    expect(product.name).toBe(randomProduct)
   })
 })
