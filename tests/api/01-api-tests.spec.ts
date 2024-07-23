bc369ae133c613fb19f4c4038b0eee795b8f16da
josh <66197125+justjoshn@users.noreply.github.com>
2024-07-23 14:16:34 -0700

    created 6 api tests

--- tests/api/01-api-tests.spec.ts
+++ tests/api/01-api-tests.spec.ts
@@ -4,13 +4,54 @@ import { faker } from '@faker-js/faker'
 const productsList = '/api/productsList'
 const brandsList = '/api/brandsList'
 const searchProduct = '/api/searchProduct'
-const notSupportedResponseMessage = 'This request method is not supported.'
+const verifyLogin = '/api/verifyLogin'
+const createAccount = '/api/createAccount'
+const deleteAccount = '/api/deleteAccount'
+const updateAccount = '/api/updateAccount'
+const getUserDetailByEmail = '/api/getUserDetailByEmail'
+const requestMethodMessage = 'This request method is not supported.'
+const email = 'firstName@email.com'
+const password = 'password'
+const randomFullName = faker.person.fullName()
+const randomEmail = faker.internet.email()
+const randomTitle = faker.helpers.arrayElement(['Mr.', 'Mrs.'])
+
+const randomDate = faker.date.birthdate({
+  min: 1982,
+  max: 2006,
+  mode: 'year',
+})
+
+const randomPassword = faker.internet.password()
+const randomDay = randomDate.getDate().toString()
+const randomMonth = (randomDate.getMonth() + 1).toString()
+const randomYear = randomDate.getFullYear().toString()
+const randomFirstName = faker.person.firstName()
+const randomLastName = faker.person.lastName()
+const randomCompany = faker.company.name()
+const randomAddress1 = faker.location.streetAddress()
+const randomAddress2 = faker.location.secondaryAddress()
+
+const randomCountry = faker.helpers.arrayElement([
+  'India',
+  'United States',
+  'Canada',
+  'Australia',
+  'Israel',
+  'New Zealand',
+  'Singapore',
+])
+
+const randomState = faker.location.state()
+const randomCity = faker.location.city()
+const randomZipCode = faker.location.zipCode()
+const randomPhoneNumber = faker.phone.number()
 
 test('Get All Products List', async ({ request }) => {
   const response = await request.get(productsList)
   const jsonResponse = await response.json()
 
-  expect(response.status()).toBe(200)
+  expect(jsonResponse.responseCode).toBe(200)
   expect(jsonResponse).toHaveProperty('products')
   expect(Array.isArray(jsonResponse.products)).toBe(true)
   expect(jsonResponse.products.length).toBeGreaterThan(0)
@@ -41,14 +82,14 @@ test('POST To All Products List', async ({ request }) => {
   const jsonResponse = await response.json()
 
   expect(jsonResponse.responseCode).toBe(405)
-  expect(jsonResponse.message).toBe(notSupportedResponseMessage)
+  expect(jsonResponse.message).toBe(requestMethodMessage)
 })
 
 test('Get All Brands List', async ({ request }) => {
   const response = await request.get(brandsList)
   const jsonResponse = await response.json()
 
-  expect(response.status()).toBe(200)
+  expect(jsonResponse.responseCode).toBe(200)
   expect(jsonResponse).toHaveProperty('brands')
   expect(Array.isArray(jsonResponse.brands)).toBe(true)
   expect(jsonResponse.brands.length).toBeGreaterThan(0)
@@ -77,7 +118,7 @@ test('PUT To All Brands List', async ({ request }) => {
   const jsonResponse = await response.json()
 
   expect(jsonResponse.responseCode).toBe(405)
-  expect(jsonResponse.message).toBe(notSupportedResponseMessage)
+  expect(jsonResponse.message).toBe(requestMethodMessage)
 })
 
 test('POST To Search Product', async ({ request }) => {
@@ -95,9 +136,9 @@ test('POST To Search Product', async ({ request }) => {
     },
   })
 
-  expect(searchResponse.status()).toBe(200)
-
   const searchResults = await searchResponse.json()
+
+  expect(searchResults.responseCode).toBe(200)
 
   searchResults.products.forEach(product => {
     expect(product).toHaveProperty('name')
@@ -109,11 +150,134 @@ test('POST To Search Product without search_product parameter', async ({ request
   const response = await request.post(searchProduct)
   const jsonResponse = await response.json()
 
-  console.log(jsonResponse)
-
   expect(jsonResponse.responseCode).toBe(400)
 
   expect(jsonResponse.message).toBe(
     'Bad request, search_product parameter is missing in POST request.'
   )
+})
+
+test('POST To Verify Login with valid details', async ({ request }) => {
+  const response = await request.post(verifyLogin, {
+    form: {
+      email: email,
+      password: password,
+    },
+  })
+
+  const jsonResponse = await response.json()
+
+  expect(jsonResponse.responseCode).toBe(200)
+  expect(jsonResponse.message).toBe('User exists!')
+})
+
+test('POST To Verify Login without email parameter', async ({ request }) => {
+  const response = await request.post(verifyLogin, {
+    form: {
+      password: password,
+    },
+  })
+
+  const jsonResponse = await response.json()
+
+  expect(jsonResponse.responseCode).toBe(400)
+
+  expect(jsonResponse.message).toBe(
+    'Bad request, email or password parameter is missing in POST request.'
+  )
+})
+
+test('DELETE To Verify Login', async ({ request }) => {
+  const response = await request.delete(verifyLogin, {
+    form: {
+      email: email,
+      password: password,
+    },
+  })
+
+  const jsonResponse = await response.json()
+
+  expect(jsonResponse.responseCode).toBe(405)
+  expect(jsonResponse.message).toBe(requestMethodMessage)
+})
+
+test('POST To Verify Login with invalid details', async ({ request }) => {
+  const response = await request.post(verifyLogin, {
+    form: {
+      email: randomEmail,
+      password: randomPassword,
+    },
+  })
+
+  const jsonResponse = await response.json()
+
+  expect(jsonResponse.responseCode).toBe(404)
+  expect(jsonResponse.message).toBe('User not found!')
+})
+
+test('POST To Create/Register User Account', async ({ request }) => {
+  const createUserResponse = await request.post(createAccount, {
+    form: {
+      name: randomFullName,
+      email: randomEmail,
+      password: randomPassword,
+      title: randomTitle,
+      birth_date: randomDate.getDate().toString(),
+      birth_month: (randomDate.getMonth() + 1).toString(),
+      birth_year: randomDate.getFullYear().toString(),
+      firstname: randomFirstName,
+      lastname: randomLastName,
+      company: randomCompany,
+      address1: randomAddress1,
+      address2: randomAddress2,
+      country: randomCountry,
+      zipcode: randomZipCode,
+      state: randomState,
+      city: randomCity,
+      mobile_number: randomPhoneNumber,
+    },
+  })
+
+  const createUserJson = await createUserResponse.json()
+
+  expect(createUserJson.responseCode).toBe(201)
+  expect(createUserJson.message).toBe('User created!')
+})
+
+test('DELETE METHOD To Delete User Account', async ({ request }) => {
+  const createUserResponse = await request.post(createAccount, {
+    form: {
+      name: randomFullName,
+      email: randomEmail,
+      password: randomPassword,
+      title: randomTitle,
+      birth_date: randomDate.getDate().toString(),
+      birth_month: (randomDate.getMonth() + 1).toString(),
+      birth_year: randomDate.getFullYear().toString(),
+      firstname: randomFirstName,
+      lastname: randomLastName,
+      company: randomCompany,
+      address1: randomAddress1,
+      address2: randomAddress2,
+      country: randomCountry,
+      zipcode: randomZipCode,
+      state: randomState,
+      city: randomCity,
+      mobile_number: randomPhoneNumber,
+    },
+  })
+
+  expect(createUserResponse.ok()).toBeTruthy()
+
+  const deleteUserResponse = await request.delete(deleteAccount, {
+    form: {
+      email: randomEmail,
+      password: randomPassword,
+    },
+  })
+
+  const deleteUserJson = await deleteUserResponse.json()
+
+  expect(deleteUserJson.responseCode).toBe(200)
+  expect(deleteUserJson.message).toBe('Account deleted!')
 })
